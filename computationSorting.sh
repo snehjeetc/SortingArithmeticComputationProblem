3e235ec3dcc1ca8c897dd75ceeea8bfe242f3eb7
snehjeetc12 <csnehjeet12@gmail.com>
2021-02-08 16:48:42 +0530

    fourth computation added

--- computationSorting.sh
+++ computationSorting.sh
@@ -5,13 +5,22 @@ declare -i b=0
 declare -i c=0
 declare -i res1=0
 declare -i res2=0
+declare -i res3=0
 
 TakeInput(){
     read -p "Enter three numbers : " a b c
 }
+
+
 compute(){
     res1=$(($a+$b*$c))
     res2=$(($a*$b+$c))
+    if [ $b -eq 0 ]
+    then
+        echo "Error Divide by zero"
+        exit -1
+    fi
+    res3=$(($c+($a/$b)))
 }
 
 
