b3334674182aaff632a4b69aba3d51660ffa26d0
snehjeetc12 <csnehjeet12@gmail.com>
2021-02-08 16:11:22 +0530

    second computation done

--- computationSorting.sh
+++ computationSorting.sh
@@ -3,13 +3,15 @@
 declare -i a=0
 declare -i b=0
 declare -i c=0
-declare -i res=0
+declare -i res1=0
+declare -i res2=0
 
 TakeInput(){
     read -p "Enter three numbers : " a b c
 }
 compute(){
-    res=$(($a+$b*$c))
+    res1=$(($a+$b*$c))
+    res2=$(($a*$b+$c))
 }
 
 
