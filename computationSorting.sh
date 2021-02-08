9b3ff97091cc4367f22538a0524d5d5189933adf
snehjeetc12 <csnehjeet12@gmail.com>
2021-02-08 17:03:49 +0530

    Results stored in dictionary

--- computationSorting.sh
+++ computationSorting.sh
@@ -3,10 +3,13 @@
 declare -i a=0
 declare -i b=0
 declare -i c=0
-declare -i res1=0
-declare -i res2=0
-declare -i res3=0
-declare -i res4=0
+declare -A results
+declare -i key=0
+
+storeInDictionary(){
+    results[$key]=$1
+    ((key++))
+}
 
 TakeInput(){
     read -p "Enter three numbers : " a b c
@@ -15,14 +18,18 @@ TakeInput(){
 
 compute(){
     res1=$(($a+$b*$c))
+    storeInDictionary $res1
     res2=$(($a*$b+$c))
+    storeInDictionary $res2
     if [ $b -eq 0 ]
     then
         echo "Error Divide by zero"
         exit -1
     fi
     res3=$(($c+($a/$b)))
+    storeInDictionary $res3
     res4=$(($a%$b + $c))
+    storeInDictionary $res4
 }
 
 
