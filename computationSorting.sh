518d6099d53cf8590b3f855eefbe998977867581
snehjeetc12 <csnehjeet12@gmail.com>
2021-02-08 16:01:58 +0530

    Added input functionality

--- computationSorting.sh
+++ computationSorting.sh
@@ -1,4 +1,13 @@
 #!/bin/bash -x
 
-echo "This problem computes different arithmetic expressions and Sorts the results"
+declare -i a=0
+declare -i b=0
+declare -i c=0
 
+TakeInput(){
+    read -p "Enter three numbers : " a b c
+}
+
+
+echo "This problem computes different arithmetic expressions and Sorts the results"
+TakeInput
