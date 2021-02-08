da8afae223a1619da6e12d3f451faef2eea46706
snehjeetc12 <csnehjeet12@gmail.com>
2021-02-08 16:07:16 +0530

    Added first computation

--- computationSorting.sh
+++ computationSorting.sh
@@ -3,11 +3,16 @@
 declare -i a=0
 declare -i b=0
 declare -i c=0
+declare -i res=0
 
 TakeInput(){
     read -p "Enter three numbers : " a b c
+}
+compute(){
+    res=$(($a+$b*$c))
 }
 
 
 echo "This problem computes different arithmetic expressions and Sorts the results"
 TakeInput
+compute
